83a6b7439f8e280fb7f7947fc6c5960ec58325ae
Ana Gaitan <anais6622@hotmail.com>
2022-11-30 20:12:29 -0500

    Se inicia proyecto con framework Nest

---
+++ src/users/schema/users.schema.ts
@@ -0,0 +1,21 @@
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop()
+  last_name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schema/users.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private usersModel: Model<UserDocument>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.usersModel.find();
+  }
+
+  findOne(id: string) {
+    return this.usersModel.findOne({ _id: id });
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
